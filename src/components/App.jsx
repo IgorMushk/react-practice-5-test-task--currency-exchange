aa65272d0b9b0bcc5f01061229444946f512ccad
IgorMushk <igor.mushketik@gmail.com>
2024-01-07 16:05:23 +0200

    When loading the page you need to get the user's current currency - getCurrentPosition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,37 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useActionData } from "react-router-dom";
 import { LayoutHome } from "./LayoutHome";
 import HomePage from "pages/HomePage/HomePage";
 import RatesPage from "pages/RatesPage/RatesPage";
+import { useEffect } from "react";
+import { getCurrentPosition } from "service/getCurrentPosition";
 
 export const App = () => {
+
+  useEffect(()=> {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    
+    function success(pos) {
+      const crd = pos.coords;
+      getCurrentPosition(pos.coords)
+      
+      // console.log("Your current position is:");
+      // console.log(`Latitude : ${crd.latitude}`);
+      // console.log(`Longitude: ${crd.longitude}`);
+      // console.log(`More or less ${crd.accuracy} meters.`);
+    }
+    
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    navigator.geolocation.getCurrentPosition(success, error, options);
+    
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<LayoutHome/>}> 
---
+++ src/service/getCurrentPosition.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+// fixed warning
+
+export const getCurrentPosition = async ({ latitude, longitude }) => {
+  const apiKey = '7b1eb8fcc52c45d3a605bf55a79da5f2';
+  //const apiKey = 'd4683b09d0c94ec0aebf0b2e043decbf';
+  // const apiKey = 'tzDy6aIAhsw5ghil4aQ4S69bGDYgTFy5';
+  const urlPosition = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}&language=en`;
+
+  const { data } = await axios.get(urlPosition);
+  console.log(data);
+  return data;
+};
