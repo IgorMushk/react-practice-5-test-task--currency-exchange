d877ac5d844ea3ec4c4ae07c5c5281e7f8189358
IgorMushk <igor.mushketik@gmail.com>
2024-01-10 18:38:06 +0200

    fix - Line 4:10:  'exchangeCurrency' is defined but never used  no-unused-vars

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchChangeCurrency } from 'redux/operations';
-import { exchangeCurrency } from 'service/chendeCurrency';
+//import { exchangeCurrency } from 'service/chendeCurrency';
 
 export const Form = () => {
   const dispatch = useDispatch()
