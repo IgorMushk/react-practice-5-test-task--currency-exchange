0d8cc346685a43fd91c6aa547477ae08155a5892
IgorMushk <igor.mushketik@gmail.com>
2024-01-10 17:08:55 +0200

    Handling a form event

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { exchangeCurrency } from 'service/chendeCurrency';
 
 export const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const {value} = e.target.elements.currency;
+    //console.log(e.target.elements.currency.value);
+    //console.log(value.split(' ')); 
+    const [amount, from, ,to] =value.split(' ');
+    //console.log({amount, from, to});
+    exchangeCurrency({amount, from, to})
+  } 
   return (
     // <div>Form</div>
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <label>
-        <input type="text" placeholder='15 USD in UAH'/>
+        <input type="text" name="currency" placeholder='15 USD in UAH'/>
       </label>
       <button type="submit">Change</button>
     </form>
